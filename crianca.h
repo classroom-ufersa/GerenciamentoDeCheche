99a265e0197908253081139b8bd504b4fe6d527e
Leandro Carlos <147527757+LeandroCarl@users.noreply.github.com>
2024-03-21 20:06:15 +0000

    Criação dos arquivos .h

---
+++ crianca.h
@@ -0,0 +1,4 @@
+typedef struct crianca Crianca;
+
+Crianca *adiciona_crianca(char nome[50], int idade, char doc[50], char sexo[10]);
+
---
+++ responsa.h
@@ -0,0 +1,7 @@
+typedef struct responsavel Responsas;
+typedef struct lstCrianca LstCrianca;
+
+Responsas *adicionar_responsavel(char nome[100], int telefone);
+
+
+
